8bf50532fd9bcc863c74712e69f86798520f6b6c
quintinbarry <55253938+quintinbarry@users.noreply.github.com>
2020-07-03 23:15:00 -0400

    Update run_me.py

--- src/run_me.py
+++ src/run_me.py
@@ -5,83 +5,46 @@ import copy
 import sys
 from sklearn.cluster import KMeans
 
-
-
-def visualize(im1, im2, k):
-    # displays two images
-    im1 = im1.astype('uint8')
-    im2 = im2.astype('uint8')
-    f = plt.figure()
-    f.add_subplot(1, 2, 1)
-    plt.imshow(im1)
-    plt.axis('off')
-    plt.title('Original')
-    f.add_subplot(1, 2, 2)
-    plt.imshow(im2)
-    plt.axis('off')
-    plt.title('Cluster: ' + str(k))
-    plt.savefig('k_means_' + str(k) + '.jpg')
-    plt.show()
-    return None
-
-
-def MSE(Im1, Im2):
-    # computes error
-    Diff_Im = Im2 - Im1
-    Diff_Im = np.power(Diff_Im, 2)
-    Diff_Im = np.sum(Diff_Im)
-    Diff_Im = np.sqrt(Diff_Im)
-    sum_diff = np.sum(np.sum(Diff_Im))
-    avg_error = sum_diff / float(Im1.shape[0] * Im2.shape[1])
-    return avg_error
-
-
-def question3(data, original):
-    k = [2,5,10,25,50,100,200]
-    for j in k:
-        guesser = KMeans(n_clusters=j)
-        temp = (guesser.fit_predict(data))
-        changed = []
-        for i in temp:
-            changed.append([guesser.cluster_centers_[i][0], guesser.cluster_centers_[i][1], guesser.cluster_centers_[i][2]])
-
-        changed = np.array(changed)
-        changed = changed.reshape(309, 393, 3)
-
-        original_image = changed
-        visualize(original, original_image,j)
-        # original_image = original_image.astype('uint8')
-        # plt.imshow(original_image)
-        # plt.show()
-        print("error for ", j, MSE(original, changed))
+################################################################################
+##@function- displays the top K rank approximation of input image
+##@param image- input image to be approximated
+##@param vectors- the eigenvectors of image, in order of decreasing eigenvaules
+##@return- void, simply displays the approximations of the image along with
+##           the MSE of each approximation
+################################################################################
 
 def PCA(image, vectors):
-    k = [6, 12, 18, 24, 30, 36, 42, 50]
+    k = [6, 12, 18, 24, 30, 36, 42, 50]#ranks to be approximated
     for j in k:
         proj = []
-        for i in range(j):
+        for i in range(j):#getting the top k eigenvectors 
             proj.append(vectors[i])
         proj = np.array(proj)
         proj = proj.transpose()
-        reduced = (image@proj)@proj.transpose()
+        reduced = (image@proj)@proj.transpose()#projecting onto the top K rank approx subspace
         #visualize(image, reduced, j)
-        plt.imshow(reduced)
+        plt.imshow(reduced)#showing the image
         plt.title('Rank '+ str(j)+ " approximation")
         plt.show()
         print('error for ', j, np.square(np.subtract(image, reduced)).mean())
 
+        
+        
+####################################################################
+##@function- calculates the average MSE of PCA on all 100 images
+####################################################################
 def PCAmse():
     k = [6, 12, 18, 24, 30, 36, 42, 50]
     output=[0,0,0,0,0,0,0,0]
     for l in range(100):
-        base = '../../Data/Faces/face_'
+        base = '../../Data/Faces/face_'#reading in each picture one at a time
         f = Image.open(base + str(l) + '.png')
         image = np.array(f)
-        image=image/np.linalg.norm(image)
-        comatrix = image @ image.transpose()
-        values, vectors = np.linalg.eigh(comatrix)
-        #print(values[0])
-        vectors = np.flip(vectors)
+        image=image/np.linalg.norm(image)#normalizing image matrix
+        comatrix = image @ image.transpose()#creating covariance matrix
+        values, vectors = np.linalg.eigh(comatrix)#get eigenvectors 
+        vectors = np.flip(vectors)#order eigenvectors such that the one corresponding to the highest eigenvalue is first
+        #Perform PCA for each image
         for j in range(len(k)):
             proj = []
             for i in range(k[j]):
@@ -89,57 +52,32 @@ def PCAmse():
             proj = np.array(proj)
             proj = proj.transpose()
             reduced = (image @ proj) @ proj.transpose()
-            # visualize(image, reduced,j)
-            # print('error for ', j, MSE(image, reduced))
-            output[j]+=np.square(np.subtract(image, reduced)).mean()
-    #output.insert(0,100)
+            output[j]+=np.square(np.subtract(image, reduced)).mean()#calculate MSE for each image
     output = np.array(output)
     output = output/100
-    # plt.plot([6, 12, 18, 24, 30, 36, 42, 50], output)
-    # plt.xlabel('k')
-    # plt.ylabel('avg MSE')
-    # plt.title('Avg MSE for each K for all 100 faces')
-    # plt.show()
+    #plotting MSE by K
+    plt.plot([6, 12, 18, 24, 30, 36, 42, 50], output)
+    plt.xlabel('k')
+    plt.ylabel('avg MSE')
+    plt.title('Avg MSE for each K for all 100 faces')
+    plt.show()
 
 
 # grab the image
 PCAmse()
 
-original_image = np.array(Image.open('../../Data/face.png'))
-test = original_image.astype('uint8')
+original_image = np.array(Image.open('../../Data/face.png')) #read in example image
+test = original_image.astype('uint8')#showing original image
 plt.imshow(test)
 plt.show()
-# #original_image = original_image.reshape(2500,1)
-# # print(np.shape(original_image))
-# # print(original_image)
-# data = []
-# y=[]
-original_image = original_image/np.linalg.norm(original_image)
-test = original_image.astype('uint8')
-plt.imshow(test)
-plt.show()
-comatrix = original_image@original_image.transpose()
-# # print(np.shape(comatrix))
-values, vectors = np.linalg.eigh(comatrix)
-vectors = np.flip(vectors)
-#print(np.flip(values))
-plt.plot(list(range(0,50)), np.flip(values))
+original_image = original_image/np.linalg.norm(original_image)#normalizing image matrix
+comatrix = original_image@original_image.transpose()#creating covariance matrix
+values, vectors = np.linalg.eigh(comatrix)#getting eigenvectors
+vectors = np.flip(vectors)#reordering eigenvectors in decreasing order
+plt.plot(list(range(0,50)), np.flip(values))#plotting eigenvalues in decreasing order, this tells you how well a matrix can be approximated. A steep drop off early on is desired
 plt.xlabel('k')
 plt.ylabel('avg MSE')
 plt.title('Avg MSE for each K for all 100 faces')
 plt.show()
 
 PCA(original_image, vectors)
-
-
-# original_image = original_image.astype('uint8')
-# plt.imshow(original_image)
-# plt.show()
-# print(np.shape(original_image))
-# for i in range(309):
-#     for j in range(393):
-#         data.append(original_image[i][j])
-
-#test = np.array(data).reshape(309,393,3)
-#question3(data, original_image)
-
